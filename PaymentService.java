3bee83435bb94bb52cecdc9cbd7c2dfb781b3b1f
Zhanarys04 <zhanarys58@gmail.com>
2024-11-13 10:08:20 +0500

    Create PaymentService.java

---
+++ PaymentService.java
@@ -0,0 +1,9 @@
+public class PaymentService {
+    public void processPayment() {
+        System.out.println("Processing payment...");
+    }
+
+    public void verifyPayment() {
+        System.out.println("Verifying payment...");
+    }
+}
