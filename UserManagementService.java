0480b2708398c7df058041867751f1192042ffd0
Zhanarys04 <zhanarys58@gmail.com>
2024-11-13 10:09:37 +0500

    Create UserManagementService.java

---
+++ UserManagementService.java
@@ -0,0 +1,9 @@
+public class UserManagementService {
+    public void registerUser(String username, String password) {
+        System.out.println("User registered: " + username);
+    }
+
+    public void loginUser(String username, String password) {
+        System.out.println("User logged in: " + username);
+    }
+}
