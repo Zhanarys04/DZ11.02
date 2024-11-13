6af1428b9a19a219ccdd7e53b01ddc82b4cdd9ad
Zhanarys04 <zhanarys58@gmail.com>
2024-11-13 10:07:20 +0500

    Create HotelService.java

---
+++ HotelService.java
@@ -0,0 +1,9 @@
+public class HotelService {
+    public void findHotelsByCriteria(String location) {
+        System.out.println("Searching hotels in " + location);
+    }
+
+    public void getHotelInfo(String hotelName) {
+        System.out.println("Hotel information for " + hotelName);
+    }
+}
