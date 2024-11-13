837cfcf694814323203e62990f32130c69224038
Zhanarys04 <zhanarys58@gmail.com>
2024-11-13 10:07:47 +0500

    Create NotificationService.java

---
+++ NotificationService.java
@@ -0,0 +1,9 @@
+public class NotificationService {
+    public void sendBookingConfirmation() {
+        System.out.println("Booking confirmation sent.");
+    }
+
+    public void sendPaymentReminder() {
+        System.out.println("Payment reminder sent.");
+    }
+}
